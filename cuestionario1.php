cb67fba69cb6821df5fa5dfff12a045071daafe9
Rene Ramirez Rendon <ShinySick@MacBook-Pro-de-Rene.local>
2018-04-17 19:16:14 -0500

    Pregunta 14

--- cuestionario1.php
+++ cuestionario1.php
@@ -22,77 +22,77 @@
 			<input type="radio" name="q2" value="4"> 4
   			<input type="radio" name="q2" value="5"> 5 <br><br>
 		  <label>03. Generalmente trato de usar estrategias que me han funcionado antes</label><br>
-		  <input type="radio" name="q3" value="1"> 1
-  			<input type="radio" name="q3" value="2"> 2
-  			<input type="radio" name="q3" value="3"> 3
-			<input type="radio" name="q3" value="4"> 4
-  			<input type="radio" name="q3" value="5"> 5 <br><br>
+	    <input type="radio" name="q3" value="1"> 1
+		<input type="radio" name="q3" value="2"> 2
+		<input type="radio" name="q3" value="3"> 3
+		<input type="radio" name="q3" value="4"> 4
+		<input type="radio" name="q3" value="5"> 5 <br><br>
 		  <label>04. Generalmente me administro mientras aprendo para tener tiempo suficiente</label><br>
-		  <input type="radio" name="q4" value="1"> 1
-  			<input type="radio" name="q4" value="2"> 2
-  			<input type="radio" name="q4" value="3"> 3
-			<input type="radio" name="q4" value="4"> 4
-  			<input type="radio" name="q4" value="5"> 5 <br><br>
+	    <input type="radio" name="q4" value="1"> 1
+		<input type="radio" name="q4" value="2"> 2
+		<input type="radio" name="q4" value="3"> 3
+		<input type="radio" name="q4" value="4"> 4
+		<input type="radio" name="q4" value="5"> 5 <br><br>
 		  <label>05. Conozco mis fortalezas y debilidades intelectuales</label><br>
-		  <input type="radio" name="q5" value="1"> 1
-  			<input type="radio" name="q5" value="2"> 2
-  			<input type="radio" name="q5" value="3"> 3
-			<input type="radio" name="q5" value="4"> 4
-  			<input type="radio" name="q5" value="5"> 5 <br><br>
+	    <input type="radio" name="q5" value="1"> 1
+		<input type="radio" name="q5" value="2"> 2
+		<input type="radio" name="q5" value="3"> 3
+		<input type="radio" name="q5" value="4"> 4
+		<input type="radio" name="q5" value="5"> 5 <br><br>
 		  <label>06. Antes de comenzar una tarea: generalmente pienso acerca de lo que realmente necesito aprender</label><br>
-		  <input type="radio" name="q6" value="1"> 1
-  			<input type="radio" name="q6" value="2"> 2
-  			<input type="radio" name="q6" value="3"> 3
-			<input type="radio" name="q6" value="4"> 4
-  			<input type="radio" name="q6" value="5"> 5 <br><br>
+	    <input type="radio" name="q6" value="1"> 1
+		<input type="radio" name="q6" value="2"> 2
+		<input type="radio" name="q6" value="3"> 3
+		<input type="radio" name="q6" value="4"> 4
+		<input type="radio" name="q6" value="5"> 5 <br><br>
 		  <label>07. Una vez que termino un examen: generalmente se que tan bien lo hice</label><br>
-		  <input type="radio" name="q7" value="1"> 1
-  			<input type="radio" name="q7" value="2"> 2
-  			<input type="radio" name="q7" value="3"> 3
-			<input type="radio" name="q7" value="4"> 4
-  			<input type="radio" name="q7" value="5"> 5 <br><br>
+	    <input type="radio" name="q7" value="1"> 1
+		<input type="radio" name="q7" value="2"> 2
+		<input type="radio" name="q7" value="3"> 3
+		<input type="radio" name="q7" value="4"> 4
+		<input type="radio" name="q7" value="5"> 5 <br><br>
 		  <label>08. Antes de comenzar una tarea: generalmente establezco metas especificas</label><br>
-		  <input type="radio" name="q8" value="1"> 1
-  			<input type="radio" name="q8" value="2"> 2
-  			<input type="radio" name="q8" value="3"> 3
-			<input type="radio" name="q8" value="4"> 4
-  			<input type="radio" name="q8" value="5"> 5 <br><br>
+	    <input type="radio" name="q8" value="1"> 1
+		<input type="radio" name="q8" value="2"> 2
+		<input type="radio" name="q8" value="3"> 3
+		<input type="radio" name="q8" value="4"> 4
+		<input type="radio" name="q8" value="5"> 5 <br><br>
 		  <label>09. Al encontrar información importante: generalmente me voy más despacio</label><br>
-		  <input type="radio" name="q9" value="1"> 1
-  			<input type="radio" name="q9" value="2"> 2
-  			<input type="radio" name="q9" value="3"> 3
-			<input type="radio" name="q9" value="4"> 4
-  			<input type="radio" name="q9" value="5"> 5 <br><br>
+	    <input type="radio" name="q9" value="1"> 1
+		<input type="radio" name="q9" value="2"> 2
+		<input type="radio" name="q9" value="3"> 3
+		<input type="radio" name="q9" value="4"> 4
+		<input type="radio" name="q9" value="5"> 5 <br><br>
 		  <label>10. Generalmente se que clase de información es la más importante para aprender</label><br>
-		  <input type="radio" name="q10" value="1"> 1
-  			<input type="radio" name="q10" value="2"> 2
-  			<input type="radio" name="q10" value="3"> 3
-			<input type="radio" name="q10" value="4"> 4
-  			<input type="radio" name="q10" value="5"> 5 <br><br>
+	    <input type="radio" name="q10" value="1"> 1
+		<input type="radio" name="q10" value="2"> 2
+		<input type="radio" name="q10" value="3"> 3
+		<input type="radio" name="q10" value="4"> 4
+		<input type="radio" name="q10" value="5"> 5 <br><br>
 		  <label>11. Al estar resolviendo un problema: generalmente me pregunto ¿si estoy  considerado todas las opciones?</label><br>
-		  <input type="radio" name="q10" value="1"> 1
-  			<input type="radio" name="q10" value="2"> 2
-  			<input type="radio" name="q10" value="3"> 3
-			<input type="radio" name="q10" value="4"> 4
-  			<input type="radio" name="q10" value="5"> 5 <br><br>
+		  <input type="radio" name="q11" value="1"> 1
+  			<input type="radio" name="q11" value="2"> 2
+  			<input type="radio" name="q11" value="3"> 3
+			<input type="radio" name="q11" value="4"> 4
+  			<input type="radio" name="q11" value="5"> 5 <br><br>
 		  <label>12. Soy bueno para organizar información</label><br>
-		  <input type="radio" name="q10" value="1"> 1
-  			<input type="radio" name="q10" value="2"> 2
-  			<input type="radio" name="q10" value="3"> 3
-			<input type="radio" name="q10" value="4"> 4
-  			<input type="radio" name="q10" value="5"> 5 <br><br>
+		  <input type="radio" name="q12" value="1"> 1
+  			<input type="radio" name="q12" value="2"> 2
+  			<input type="radio" name="q12" value="3"> 3
+			<input type="radio" name="q12" value="4"> 4
+  			<input type="radio" name="q12" value="5"> 5 <br><br>
 		  <label>13. Generalmente enfoco conscientemente mi atención en información importante</label><br>
-		  <input type="radio" name="q10" value="1"> 1
-  			<input type="radio" name="q10" value="2"> 2
-  			<input type="radio" name="q10" value="3"> 3
-			<input type="radio" name="q10" value="4"> 4
-  			<input type="radio" name="q10" value="5"> 5 <br><br>
+		  <input type="radio" name="q13" value="1"> 1
+  			<input type="radio" name="q13" value="2"> 2
+  			<input type="radio" name="q13" value="3"> 3
+			<input type="radio" name="q13" value="4"> 4
+  			<input type="radio" name="q13" value="5"> 5 <br><br>
 		  <label>14. Generalmente tengo un propósito específico para cada estrategia que uso</label><br>
-		  <input type="radio" name="q10" value="1"> 1
-  			<input type="radio" name="q10" value="2"> 2
-  			<input type="radio" name="q10" value="3"> 3
-			<input type="radio" name="q10" value="4"> 4
-  			<input type="radio" name="q10" value="5"> 5 <br><br>
+		  <input type="radio" name="q14" value="1"> 1
+  			<input type="radio" name="q14" value="2"> 2
+  			<input type="radio" name="q14" value="3"> 3
+			<input type="radio" name="q14" value="4"> 4
+  			<input type="radio" name="q14" value="5"> 5 <br><br>
 		  <label>10. Generalmente se que clase de información es la más importante para aprender</label><br>
 		  <input type="radio" name="q10" value="1"> 1
   			<input type="radio" name="q10" value="2"> 2
